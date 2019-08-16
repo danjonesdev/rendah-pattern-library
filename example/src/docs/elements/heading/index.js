00f509716fa658d0657231395d678d9cd724f607
dan jones <dannyjones360@gmail.com>
2019-08-16 08:42:43 +0100

    Setup parallax ready for functional

--- example/src/docs/elements/heading/index.js
+++ example/src/docs/elements/heading/index.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import { Heading } from 'rendah-pattern-library'
+import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
 
 import PreviewWrapper from '../../../components/preview-wrapper'
 import propDocs from './propDocs'
 
 export default function HeadingPreview(props) {
+
+  console.log('ParallaxProvider', ParallaxProvider);
   return (
     <React.Fragment>
-      <PreviewWrapper
-        title={'Heading'}
-        description={'These are the icons'}
-        propDocs={propDocs}
-      />
+      <ParallaxProvider>
 
-      <div className='container  mla  mra'>
+        <PreviewWrapper
+          title={'Heading'}
+          description={'These are the icons'}
+          propDocs={propDocs}
+        />
+
+      <div className='container  mla  mra  relative'>
         <div className={'flex  flex-wrap  mb4'}>
           <Heading
             htmlEntity={'h1'}
@@ -51,6 +56,21 @@ export default function HeadingPreview(props) {
           />
         </div>
       </div>
+
+      <div className="absolute top left mt6  pt6   w-100">
+        <Parallax className="custom-class absolute" y={[0, -200]} tagOuter="figure">
+          <img className="" src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" />
+        </Parallax>
+        <Parallax className="custom-class absolute" y={[0, 200]} tagOuter="figure">
+          <img className="" src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" />
+        </Parallax>
+      </div>
+
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+      </ParallaxProvider>
     </React.Fragment>
   )
 }
--- example/src/index.js
+++ example/src/index.js
@@ -4,4 +4,6 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+                <App />
+          , document.getElementById("root"));
