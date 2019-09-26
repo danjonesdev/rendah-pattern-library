ddc8fd6b1d0a7978c7673c75ffe6bea073cf0264
danjonesdev <dannyjones360@gmail.com>
2019-09-26 13:54:03 +0100

    Updates

---
+++ precache-manifest.f7ad663ae073bf436fc2198df232c04e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8935171d2699940b3624ed17f4c6a027",
+    "url": "/rendah-pattern-library/index.html"
+  },
+  {
+    "revision": "29d3c7d8bb3ce1c33ba4",
+    "url": "/rendah-pattern-library/static/css/main.81e3eb76.chunk.css"
+  },
+  {
+    "revision": "5ac2a998e8f0804cf28b",
+    "url": "/rendah-pattern-library/static/js/2.b94f049b.chunk.js"
+  },
+  {
+    "revision": "29d3c7d8bb3ce1c33ba4",
+    "url": "/rendah-pattern-library/static/js/main.728e9418.chunk.js"
+  },
+  {
+    "revision": "2b1ea8720a4d087bc0fc",
+    "url": "/rendah-pattern-library/static/js/runtime~main.af6244d6.js"
+  }
+]);
