64d4f4459738465411ab4468117e6c901d14ebaf
danjonesdev <dannyjones360@gmail.com>
2019-10-23 17:07:31 +0100

    Add more icons

--- example/src/docs/elements/icon/index.js
+++ example/src/docs/elements/icon/index.js
@@ -1,25 +1,43 @@
-import React from 'react'
-import { Icon } from 'rendah-pattern-library'
+import React from "react";
+import { Icon } from "rendah-pattern-library";
 
-import PreviewWrapper from '../../../components/preview-wrapper'
-import propDocs from './propDocs'
+import PreviewWrapper from "../../../components/preview-wrapper";
+import propDocs from "./propDocs";
 
 export default function IconPreview({ ...props }) {
   return (
     <React.Fragment>
       <PreviewWrapper
-        title={'Icon'}
-        description={'These are the icons'}
+        title={"Icon"}
+        description={"These are the icons"}
         propDocs={propDocs}
       />
 
-      <div className='container  mla  mra'>
-        <div className={'flex  flex-wrap'}>
-          <div className={'col-24  col-4-md  flex  mb4'}>
-            <Icon icon={'arrow-right'} color={'#000000'} />
+      <div className="container  mla  mra">
+        <div className={"flex  flex-wrap"}>
+          <div className={"col-24  col-4-md  col-2-lg  flex  mb4"}>
+            <Icon icon={"arrow-right"} color={"#000000"} />
+          </div>
+          <div className={"col-24  col-4-md  col-2-lg  flex  mb4"}>
+            <Icon icon={"facebook"} color={"#000000"} />
+          </div>
+          <div className={"col-24  col-4-md  col-2-lg  flex  mb4"}>
+            <Icon icon={"twitter"} color={"#000000"} />
+          </div>
+          <div className={"col-24  col-4-md  col-2-lg  flex  mb4"}>
+            <Icon icon={"instagram"} color={"#000000"} />
+          </div>
+          <div className={"col-24  col-4-md  col-2-lg  flex  mb4"}>
+            <Icon icon={"soundcloud"} color={"#000000"} />
+          </div>
+          <div className={"col-24  col-4-md  col-2-lg  flex  mb4"}>
+            <Icon icon={"discord"} color={"#000000"} />
+          </div>
+          <div className={"col-24  col-4-md  col-2-lg  flex  mb4"}>
+            <Icon icon={"youtube"} color={"#000000"} />
           </div>
         </div>
       </div>
     </React.Fragment>
-  )
+  );
 }
--- src/elements/icon/index.js
+++ src/elements/icon/index.js
@@ -1,14 +1,39 @@
-import React from 'react'
-import ArrowRight from './arrow-right'
+import React from "react";
+
+import ArrowRight from "./arrow-right";
+import Facebook from "./facebook";
+import Twitter from "./twitter";
+import Instagram from "./instagram";
+import SoundCloud from "./soundcloud";
+import Discord from "./discord";
+import Youtube from "./youtube";
 
 export default function Icon(props) {
-  const { icon } = props
+  const { icon } = props;
 
   switch (icon) {
-    case 'arrow-right':
-      return <ArrowRight {...props} />
-      break
+    case "arrow-right":
+      return <ArrowRight {...props} />;
+      break;
+    case "facebook":
+      return <Facebook {...props} />;
+      break;
+    case "twitter":
+      return <Twitter {...props} />;
+      break;
+    case "instagram":
+      return <Instagram {...props} />;
+      break;
+    case "soundcloud":
+      return <SoundCloud {...props} />;
+      break;
+    case "discord":
+      return <Discord {...props} />;
+      break;
+    case "youtube":
+      return <Youtube {...props} />;
+      break;
     default:
-      return false
+      return false;
   }
 }
