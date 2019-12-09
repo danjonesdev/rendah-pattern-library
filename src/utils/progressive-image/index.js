6c808889c788281dac9962185cf43a9126c6ec50
danjonesdev <dannyjones360@gmail.com>
2019-12-09 14:21:19 +0000

    Convert progressive-image to react hooks

--- src/utils/progressive-image/index.js
+++ src/utils/progressive-image/index.js
@@ -1,51 +1,33 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-const omit = (obj, omitKey) =>
-  Object.keys(obj).reduce((result, key) => {
-    if (key !== omitKey) {
-      result[key] = obj[key];
-    }
-    return result;
-  }, {});
+/**
+ * ProgressiveImage.
+ */
 
-export default class ProgressiveImage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { highResImageLoaded: false };
-  }
+export default function ProgressiveImage(props) {
+  const [highResImageLoaded, setHighResImageLoaded] = useState(false);
+  const { placeholder, height, alt, src } = props;
 
-  render() {
-    const { placeholder, height, alt } = this.props;
-    const { highResImageLoaded } = this.state;
-    let filteredProps = omit(this.props, "placeholder");
-
-    return (
-      <span>
-        <img
-          className="w-100  image"
-          style={{ height: `${height}px` }}
-          alt={alt}
-          src={this.props.src}
-          onLoad={() => {
-            this.setState({ highResImageLoaded: true });
-          }}
-          ref={img => {
-            this.highResImage = img;
-          }}
-        />
-        <img
-          className="image__loading"
-          alt={alt}
-          src={placeholder}
-          {...(highResImageLoaded
-            ? {
-                style: { height: `${height}px`, opacity: "0" }
-              }
-            : {
-                style: { height: `${height}px` }
-              })}
-        />
-      </span>
-    );
-  }
+  return (
+    <span>
+      <img
+        className="w-100  image"
+        style={{ height: `${height}px` }}
+        alt={alt}
+        src={src}
+        onLoad={() => {
+          setHighResImageLoaded(true);
+        }}
+      />
+      <img
+        className="image__loading"
+        alt={alt}
+        src={placeholder}
+        style={{
+          height: `${height}px`,
+          opacity: highResImageLoaded ? "0" : "1"
+        }}
+      />
+    </span>
+  );
 }
